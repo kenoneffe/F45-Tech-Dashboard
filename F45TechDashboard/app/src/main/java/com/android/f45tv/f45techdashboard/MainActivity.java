c7a902bef4b65e8a5072479527bb379b229eaead
lixanfajardo <lixan@bywave.com.au>
2018-04-04 16:15:38 +0800

    uploaded the blank project for OJT

---
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.f45tv.f45techdashboard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LeakSun on 04/04/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
