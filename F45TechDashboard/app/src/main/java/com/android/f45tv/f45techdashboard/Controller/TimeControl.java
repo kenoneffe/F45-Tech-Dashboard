05769c90e953166356b4cc1e6cccb2ba77f71f17
woooshooo <kaykaygenise@gmail.com>
2018-04-05 14:35:16 +0800

    /

--- F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Controller/TimeControl.java
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Controller/TimeControl.java
@@ -3,16 +3,15 @@ package com.android.f45tv.f45techdashboard.Controller;
 import android.content.Context;
 import android.os.CountDownTimer;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 
-public class TimeControl extends RelativeLayout {
+public class TimeControl {
 
     Context context;
     LinearLayout timerFragment;
     TextView minutesText;
-
 
 }
