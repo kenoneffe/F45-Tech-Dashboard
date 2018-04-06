c116432587d66e14b9ced61e3b31e09ff5199231
woooshooo <kaykaygenise@gmail.com>
2018-04-06 14:25:21 +0800

    April 6 2018 Progress

--- F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -18,30 +22,64 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     BarChart barChart;
+    String[] graphLabels;
+    ArrayList<BarEntry> barEntries;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         barChart = (BarChart) findViewById(R.id.chart);
-        barChart.setDrawBarShadow(false);
-        barChart.setDrawValueAboveBar(true);
-        barChart.setMaxVisibleValueCount(50);
-        barChart.setPinchZoom(false);
+//        barChart.setDrawBarShadow(false);
+//        barChart.setDrawValueAboveBar(true);
+//        barChart.setMaxVisibleValueCount(50);
+//        barChart.setPinchZoom(false);
+        barChart.setDoubleTapToZoomEnabled(false);
         barChart.setDrawGridBackground(true);
 
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1, 40f));
-        barEntries.add(new BarEntry(2, 44f));
-        barEntries.add(new BarEntry(3, 30f));
-        barEntries.add(new BarEntry(4, 36f));
+        barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(40f,0));
+        barEntries.add(new BarEntry(30f, 0));
+        barEntries.add(new BarEntry(50f, 0));
+
+        graphLabels = new String[]{"Opened", "Solved", "Unresolved"};
 
         BarDataSet barDataSet = new BarDataSet(barEntries, "Data Set1");
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+
 
         BarData data = new BarData(barDataSet);
         data.setBarWidth(0.9f);
 
         barChart.setData(data);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new MyAxisValueFormatter(graphLabels));
+        xAxis.setGranularity(1);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
     }
+
+    public class MyAxisValueFormatter implements IAxisValueFormatter{
+        private String[] mValues;
+
+        public MyAxisValueFormatter(String[] mValues) {
+            this.mValues = mValues;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            if (value >= 0) {
+                if (mValues.length > (int) value) {
+                    return mValues[(int) value];
+                } else return "";
+            } else {
+                return "";
+            }
+        }
+
+    }
+
+
 }
