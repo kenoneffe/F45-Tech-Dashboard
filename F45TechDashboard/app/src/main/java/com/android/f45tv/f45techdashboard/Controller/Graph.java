b9402ae437fee1c3619de05530accf69315170fb
woooshooo <kaykaygenise@gmail.com>
2018-04-06 12:34:46 +0800

    Finished Views ( hardcoded )

---
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Controller/Graph.java
@@ -0,0 +1,10 @@
+package com.android.f45tv.f45techdashboard.Controller;
+import com.android.f45tv.f45techdashboard.MainActivity;
+import com.android.f45tv.f45techdashboard.R;
+import com.github.mikephil.charting.charts.BarChart;
+
+
+public class Graph extends MainActivity{
+
+
+}
--- F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
@@ -4,15 +4,44 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 /**
  * Created by LeakSun on 04/04/2018.
  */
 
 public class MainActivity extends AppCompatActivity {
-
+    BarChart barChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barChart = (BarChart) findViewById(R.id.chart);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(true);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 40f));
+        barEntries.add(new BarEntry(2, 44f));
+        barEntries.add(new BarEntry(3, 30f));
+        barEntries.add(new BarEntry(4, 36f));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Data Set1");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(0.9f);
+
+        barChart.setData(data);
     }
 }
