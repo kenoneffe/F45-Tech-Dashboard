13df55f71e02c18cb75025e36f0e2fe5df1f05d5
woooshooo <kaykaygenise@gmail.com>
2018-04-12 16:43:26 +0800

    / update

--- F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Controller/TicketVolumeController.java
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Controller/TicketVolumeController.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.android.f45tv.f45techdashboard.Interfaces.TicketVolumeInterface;
 import com.android.f45tv.f45techdashboard.R;
 
@@ -19,24 +18,24 @@ public class TicketVolumeController extends LinearLayout implements TicketVolume
         super(context);
         this.context = context;
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate((R.layout.activity_main), this);
+        inflater.inflate((R.layout.tickets), this);
         this.context = context;
         initViews();
     }
 
     private void initViews() {
         linearLayout = findViewById(R.id.ticketsCon);
-        ticketVolume = findViewById(R.id.main_ticketsToday);
-        responseTime = findViewById(R.id.avgResponse);
     }
 
     @Override
     public void setTicketVolumeText(String text) {
-        ticketVolume.setText(text+"Tickets Today");
+        ticketVolume = findViewById(R.id.main_ticketsToday);
+        ticketVolume.setText(text+" Tickets Today");
     }
 
     @Override
     public void setResponseTimeText(String text) {
+        responseTime = findViewById(R.id.avgResponse);
         responseTime.setText(text+"s Response Time.");
     }
 
--- F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/MainActivity.java
@@ -5,13 +5,11 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-<<<<<<< HEAD
 import com.android.f45tv.f45techdashboard.Controller.TicketVolumeController;
-=======
-
 import com.android.f45tv.f45techdashboard.Controller.TimerController;
->>>>>>> 5551cc286d7df642aa17f487304433d5deec6dba
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
@@ -22,9 +20,6 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-
-import com.android.f45tv.f45techdashboard.R;
-
 
 
 /**
@@ -37,54 +32,34 @@ public class MainActivity extends AppCompatActivity {
     BarChart barChart;
     String[] graphLabels;
     ArrayList<BarEntry> barEntries;
-<<<<<<< HEAD
     TextView tv;
     CountDownTimer countDownTimer;
     TicketVolumeController ticketVolumeController;
-
-=======
     TimerController timerController;
     FrameLayout timerFrame;
-    //TextView tv;
-    //CountDownTimer countDownTimer;
+    LinearLayout ticketLayout;
 
-
-
-
-
-
-
->>>>>>> 5551cc286d7df642aa17f487304433d5deec6dba
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         setContentView(R.layout.activity_main);
-        //Marquee
-        tv = (TextView) findViewById(R.id.minutesText);
         //Ticket Volume Controller
         ticketVolumeController = new TicketVolumeController(this);
-
-        //Graph
-=======
-
-        setContentView(R.layout.activity_main);
-
-        //tv = (TextView) findViewById(R.id.minutesText);
-
-        timerFrame = (FrameLayout) findViewById(R.id.timerFrame);
+        ticketLayout = findViewById(R.id.ticketFrame);
+        //Timer Controller
+        timerFrame = findViewById(R.id.timerFrame);
         timerController = new TimerController(this);
-
->>>>>>> 5551cc286d7df642aa17f487304433d5deec6dba
-        barChart = (BarChart) findViewById(R.id.chart);
+        //Graph
+        barChart = findViewById(R.id.chart);
         barChart.setDrawBarShadow(false);
         barChart.setDrawValueAboveBar(true);
         barChart.setMaxVisibleValueCount(50);
         barChart.setPinchZoom(false);
+        barChart.setFocusable(false);
         barChart.setDoubleTapToZoomEnabled(false);
         barChart.setDrawGridBackground(true);
-
         barEntries = new ArrayList<>();
         barEntries.add(new BarEntry(1, 40f));
         barEntries.add(new BarEntry(2, 44f));
@@ -95,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         BarDataSet barDataSet = new BarDataSet(barEntries, "Cells");
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
-
         BarData data = new BarData(barDataSet);
         data.setBarWidth(0.9f);
 
@@ -105,15 +79,10 @@ public class MainActivity extends AppCompatActivity {
         xAxis.setValueFormatter(new MyAxisValueFormatter(graphLabels));
         xAxis.setGranularity(1);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-
     }
 
-<<<<<<< HEAD
-    public class MyAxisValueFormatter implements IAxisValueFormatter {
-=======
-
     public class MyAxisValueFormatter implements IAxisValueFormatter{
->>>>>>> 5551cc286d7df642aa17f487304433d5deec6dba
+
         private String[] mValues;
 
         public MyAxisValueFormatter(String[] mValues) {
@@ -134,44 +103,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-<<<<<<< HEAD
-=======
+
     protected void onStart() {
         super.onStart();
 
         timerController.setTimer(TimeUnit.MINUTES.toMillis(30), 1000);
         timerFrame.addView(timerController);
-    }
 
-    /*@Override
->>>>>>> 5551cc286d7df642aa17f487304433d5deec6dba
-    protected void onStart() {
-        super.onStart();
-
-        new CountDownTimer(60000, 1000) {
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-                tv.setText("" + millisUntilFinished / 1000);
-            }
-
-            @Override
-            public void onFinish() {
-                tv.setText("Finish");
-            }
-        }.start();
-
-        marqueeView = (TextView) findViewById(R.id.marque_scrolling_text);
+        marqueeView = findViewById(R.id.marque_scrolling_text);
         Animation marqueeAnim = AnimationUtils.loadAnimation(this, R.anim.marquee_animation);
         marqueeView.startAnimation(marqueeAnim);
 
-<<<<<<< HEAD
         //onStart set Ticket Volume and Response Time
         ticketVolumeController.setTicketVolumeText("69");
         ticketVolumeController.setResponseTimeText("123");
+        ticketLayout.addView(ticketVolumeController);
     }
-=======
-    }*/
-
->>>>>>> 5551cc286d7df642aa17f487304433d5deec6dba
 }
+
