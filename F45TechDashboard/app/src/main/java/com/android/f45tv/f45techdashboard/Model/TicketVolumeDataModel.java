23f6f8f0ad5cbd2274c1206b2997ae8f5e5aa764
Webster Kyle Genise <woooshooo@users.noreply.github.com>
2018-04-26 13:02:35 +0800

    Added all POJO

--- F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Model/TicketVolumeDataModel.java
+++ F45TechDashboard/app/src/main/java/com/android/f45tv/f45techdashboard/Model/TicketVolumeDataModel.java
@@ -1,15 +1,42 @@
 package com.android.f45tv.f45techdashboard.Model;
 
 import com.google.gson.annotations.SerializedName;
+import java.util.Dictionary;
 
 public class TicketVolumeDataModel {
 
-    @SerializedName("status")
-    public String status;
-    @SerializedName("created_at")
+    public Object[] attachments;
+    public String[] cc_emails;
+    public Number company_id;
+    public Dictionary custom_fields;
+    public Boolean deleted;
+    public String description;
+    public String description_text;
+    public String due_by;
+    public String email;
+    public Number email_config_id;
+    public String facebook_id;
+    public String fr_due_by;
+    public Boolean fr_escalated;
+    public String[] fwd_emails;
+    public Number group_id;
+    public Number id;
+    public Boolean is_escalated;
+    public String name;
+    public String phone;
+    public Number priority;
+    public Number product_id;
+    public String[] reply_cc_emails;
+    public Number requester_id;
+    public Number responder_id;
+    public Number source;
+    public Boolean spam;
+    public Number status;
+    public String subject;
+    public String[] tags;
+    public String[] to_emails;
+    public String twitter_id;
+    public String type;
     public String created_at;
-    @SerializedName("updated_at")
     public String updated_at;
-    @SerializedName("id")
-    public String ticket_id;
 }
